83d44d93c6b12e7bfdc30f3b5348ccad465170b1
carrry0209 <aaa.mmm.carrry@gmail.com>
2020-03-29 11:38:15 +0900

    first commit

---
+++ src/test/java/com/carrry/uq/UqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrry.uq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
